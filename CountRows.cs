69fe92529cb85bd41f3ffc2ff9b527dbc8f6a0c3
Dhruv kushwah <dhruvkushwah243@gmail.com>
2025-02-25 22:21:42 +0530

    [Dhruv] Add. Added Assignment of 25th Feb

---
+++ CountRows.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using CsvHelper;
+using System.Globalization;
+
+
+class CountRows
+{
+   public static void Main(string[] args)
+   {
+       using (var reader = new StreamReader("D:\\Dhruv254\\Assign252\\student_details.csv"))
+       using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+       {
+           var records = csv.GetRecords<Student>();
+
+           int count = 0;
+           foreach (var record in records)
+           {
+               count++;
+           }
+           Console.WriteLine(count);
+       }
+   }
+}
+
+class Student
+{
+   public int ID { get; set; }
+   public string Name { get; set; }
+   public int Age { get; set; }
+   public int Marks { get; set; }
+}
---
+++ ReadCSV.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using CsvHelper;
+using System.Globalization;
+
+class ReadCSV
+{
+   public static void Main(string[] args)
+   {
+       using (var reader = new StreamReader("D:\\Dhruv254\\Assign252\\student_details.csv"))
+       using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+       {
+           var records = csv.GetRecords<Student>();
+
+           foreach (var record in records)
+           {
+               Console.WriteLine("ID: " + record.ID + ", Name: " + record.Name + ", Age: " + record.Age + ", Marks: " + record.Marks);
+           }
+       }
+   }
+}
+
+class Student
+{
+   public int ID { get; set; }
+   public string Name { get; set; }
+   public int Age { get; set; }
+   public int Marks { get; set; }
+}
